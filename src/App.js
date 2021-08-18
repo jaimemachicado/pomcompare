818ca61d9fd8fc027d8b7227ea86ee2e644a9245
Jaime Machicado <jaimemachicado@gmail.com>
2021-08-18 20:08:13 +0200

    POMCOM-004: Se implementa componente Nav que contiene el texto POM COMPARE que se muestra en la aplicación como título.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import FileList from "./draganddropfile/FileList";
+import NavBar from "./components/navbar/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hola mundo</h1>
-      </header>
-      <FileList>
-      </FileList>
+      <NavBar></NavBar>
     </div>
   );
 }
---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../App.css';
+
+function NavBar() {
+    return (
+        <nav className="navstyle">
+            <h1>POM COMPARE</h1>
+        </nav>
+    );
+}
+
+export default NavBar;
